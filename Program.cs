9a7063e77cf8ccb74e57fe63ef03422936fa3f27
HISOKA715 <67085413+HISOKA715@users.noreply.github.com>
2026-04-20 15:35:09 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 // Add services to the container.
 builder.Services.AddHttpClient();
@@ -25,20 +27,18 @@ builder.Services.AddSession(options =>
 builder.Services.AddHttpContextAccessor();
 
 
+
+
 var firebaseApp = FirebaseApp.Create(new AppOptions()
 {
-    Credential = GoogleCredential.FromFile("C:/Users/user/Documents/Visual Studio 2022/webCore/Controllers/fir-project-7ba4f-firebase-adminsdk-3pu6p-8d30027601.json"),
+    Credential = GoogleCredential.FromFile("firebase-key.json"),
 });
 builder.Services.AddSingleton(firebaseApp);
 
-
 builder.Services.AddSingleton<FirestoreDb>(provider =>
 {
-    string filePath = "C:/Users/user/Documents/Visual Studio 2022/webCore/Controllers/fir-project-7ba4f-firebase-adminsdk-3pu6p-8d30027601.json";
-    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filePath);
     return FirestoreDb.Create("fir-project-7ba4f");
 });
-
 
 
 var app = builder.Build();
